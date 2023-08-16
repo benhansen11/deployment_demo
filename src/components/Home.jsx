fa3985efd7a93f13e71b18402098a9a687bc385a
benhansen11 <ben1hansen@hotmail.com>
2023-08-16 17:13:00 -0600

    added padding to the home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,7 +11,8 @@ const Home = () => {
       {/* Container */}
       <Slide>
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-          <p className="py-14 text-[#0a192f]"></p>
+          <p className="py-20 text-[#0a192f]"></p>
+          <p className="py-5 text-[#0a192f]"></p>
           <p className="text-3xl text-pink-600">Hi, my name is</p>
           <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
             Ben Hansen,
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,7 +4,6 @@ import ChatApp from "../assets/chat_app.jpg";
 import Admin from "../assets/admin.png";
 import { Zoom } from "react-awesome-reveal";
 
-
 const Projects = () => {
   return (
     <div
@@ -19,77 +18,77 @@ const Projects = () => {
           <p className="py-2"> Check out some of my recent projects </p>
         </div>
         <Zoom>
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
-          <div
-            style={{ backgroundImage: `url(${Admin})` }}
-            className="shadow-lg shadow-[#040c16] group containter rounded-md flex justify-center items-center content-div px-4"
-          >
-            {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-3xl font-bold text-white tracking-wider flex">
-                Admin React Dashboard
-              </span>
-              <div className="pt-8 text-center">
-                <a href="https://ben-hansen-react-admin-dashboard.netlify.app/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="https://github.com/benhansen11/react-admin-dashboard">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
+          <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+            <div
+              style={{ backgroundImage: `url(${Admin})` }}
+              className="shadow-lg shadow-[#040c16] group containter rounded-md flex justify-center items-center content-div px-4"
+            >
+              {/* Hover Effects */}
+              <div className="opacity-0 group-hover:opacity-100">
+                <span className="text-3xl font-bold text-white tracking-wider flex">
+                  Admin React Dashboard
+                </span>
+                <div className="pt-8 text-center">
+                  <a href="https://ben-hansen-react-admin-dashboard.netlify.app/">
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Demo
+                    </button>
+                  </a>
+                  <a href="https://github.com/benhansen11/react-admin-dashboard">
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Code
+                    </button>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div
+              style={{ backgroundImage: `url(${Netflix})` }}
+              className="shadow-lg shadow-[#040c16] group containter rounded-md flex justify-center items-center content-div"
+            >
+              {/* Hover Effects */}
+              <div className="opacity-0 group-hover:opacity-100">
+                <span className="text-3xl font-bold text-white tracking-wider">
+                  Netflix React App
+                </span>
+                <div className="pt-8 text-center">
+                  <a href="https://ben-hansen-netflix-react-app.netlify.app/">
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Demo
+                    </button>
+                  </a>
+                  <a href="https://github.com/benhansen11/Netflix_Dupe">
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Code
+                    </button>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div
+              style={{ backgroundImage: `url(${ChatApp})` }}
+              className="shadow-lg shadow-[#040c16] group containter rounded-md flex justify-center items-center content-div"
+            >
+              {/* Hover Effects */}
+              <div className="opacity-0 group-hover:opacity-100">
+                <span className="text-3xl font-bold text-white tracking-wider">
+                  React Chat App
+                </span>
+                <div className="pt-8 text-center">
+                  <a href="/">
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Demo
+                    </button>
+                  </a>
+                  <a href="https://github.com/benhansen11/react_chat_app/tree/main">
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Code
+                    </button>
+                  </a>
+                </div>
               </div>
             </div>
           </div>
-          <div
-            style={{ backgroundImage: `url(${Netflix})` }}
-            className="shadow-lg shadow-[#040c16] group containter rounded-md flex justify-center items-center content-div"
-          >
-            {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-3xl font-bold text-white tracking-wider">
-                Netflix React App
-              </span>
-              <div className="pt-8 text-center">
-                <a href="https://ben-hansen-netflix-react-app.netlify.app/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="https://github.com/benhansen11/Netflix_Dupe">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          <div
-            style={{ backgroundImage: `url(${ChatApp})` }}
-            className="shadow-lg shadow-[#040c16] group containter rounded-md flex justify-center items-center content-div"
-          >
-            {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-3xl font-bold text-white tracking-wider">
-                React Chat App
-              </span>
-              <div className="pt-8 text-center">
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="https://github.com/benhansen11/react_chat_app/tree/main">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
         </Zoom>
       </div>
     </div>
